adbfed8f3d277bcc32dc6f21100be56fff1171ae
Vladimir Agafonkin <agafonkin@gmail.com>
2016-02-11 15:32:37 +0200

    add tests

---
+++ test/test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var test = require('tap').test;
+var concaveman = require('../');
+
+var points = require('./fixtures/points-1k.json');
+var hull = require('./fixtures/points-1k-hull.json');
+var hull2 = require('./fixtures/points-1k-hull2.json');
+
+test('default concave hull', function (t) {
+    var result = concaveman(points);
+    t.deepEqual(result, hull);
+    t.end();
+});
+
+test('tuned concave hull', function (t) {
+    var result = concaveman(points, 3, 0.01);
+    t.deepEqual(result, hull2);
+    t.end();
+});
